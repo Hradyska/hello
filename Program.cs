34fbe8c93fdd428f57cebae73a78f7e5eaef9dcd
Hradyska LV <l.v.hradyska@gmail.com>
2021-08-28 22:54:36 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -9,19 +9,7 @@ namespace hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
-            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-            DirectoryInfo dirInfo = new DirectoryInfo(path);
-            if (!dirInfo.Exists)
-            {
-                dirInfo.Create();
-            }
-            string text ="Hello, txt!";      
-            using (FileStream fstream = new FileStream($"{path}\note.txt", FileMode.OpenOrCreate))
-            {
-                byte[] array = System.Text.Encoding.Default.GetBytes(text);
-                // асинхронная запись массива байтов в файл
-                
-            }
+            
 	
         }
     }
