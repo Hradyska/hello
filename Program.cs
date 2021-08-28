1ff8956fed52d37dd2e904043d38294e155bce2f
Hradyska <64253822+Hradyska@users.noreply.github.com>
2021-08-28 21:43:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace hello
 {
@@ -7,6 +9,21 @@ namespace hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+            static string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            string text ="Hello!";      
+            using (FileStream fstream = new FileStream($"{path}\note.txt", FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                // асинхронная запись массива байтов в файл
+                await fstream.WriteAsync(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+	
         }
     }
 }
